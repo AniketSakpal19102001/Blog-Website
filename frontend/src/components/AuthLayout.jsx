572ea345430cd2ddef6717bb882446da9abdc93a
AniketSakpal19102001 <160087718+AniketSakpal19102001@users.noreply.github.com>
2025-01-11 00:48:37 +0530

    Update AuthLayout.jsx

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -8,8 +8,8 @@ function AuthLayout() {
     <>
       <Navbar />
       <div>{userInfo ? <Outlet /> : <Navigate to={"/"} />}</div>
-      <div className="w-full border-t py-2 bg-gray-100">
-        <div className="flex justify-center items-center gap-4 py-5">
+      <div className="w-full border-t py-2 bg-slate-50">
+        <div className="hidden md:flex justify-center items-center gap-4 py-5">
           <p className="text-gray-500 text-[0.8rem] cursor-pointer">Help</p>
           <p className="text-gray-500 text-[0.8rem] cursor-pointer">Status</p>
           <p className="text-gray-500 text-[0.8rem] cursor-pointer">About</p>
@@ -19,6 +19,9 @@ function AuthLayout() {
           <p className="text-gray-500 text-[0.8rem] cursor-pointer">Privacy</p>
           <p className="text-gray-500 text-[0.8rem] cursor-pointer">Term</p>
           <p className="text-gray-500 text-[0.8rem] cursor-pointer">Teams</p>
+        </div>
+        <div className="text-gray-500 py-3 text-[0.8rem] cursor-pointer w-full flex flex-col md:hidden items-center">
+          <p>BlogVibe @ 2025</p>
         </div>
       </div>
     </>
